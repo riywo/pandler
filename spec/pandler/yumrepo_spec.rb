b8cf39b43cb2f7857ccbb7bcb5bce0de2f23db68
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-03-26 00:56:08 -0700

    add test

--- spec/pandler/yumrepo_spec.rb
+++ spec/pandler/yumrepo_spec.rb
@@ -1,16 +1,36 @@
 require "pathname"
+require "webrick"
 
 describe Pandler::Yumrepo do
   include TempDirHelper
   before :all do
     @base_dir = File.expand_path("repo")
     @yumrepo = Pandler::Yumrepo.new(:base_dir => @base_dir)
+    test_port = 9999
 
     yumfile = <<-EOF
-repo "base",   "http://vault.centos.org/6.2/os/x86_64"
-rpm "basesystem"
+repo "test",   "http://localhost:#{test_port}"
+rpm  "pandler-test"
     EOF
     write_file("Yumfile", yumfile)
+
+    @server_thread = Thread.new do
+      server = WEBrick::HTTPServer.new(
+        :Port => test_port,
+        :Logger => WEBrick::Log.new('/dev/null'),
+        :AccessLog => [],
+        :StartCallback => Proc.new { Thread.main.wakeup },
+        :DocumentRoot => File.expand_path("../../resources/yumrepo", __FILE__)
+      )
+      Signal.trap(:INT) { server.shutdown }
+      server.start
+    end
+    Thread.stop
+  end
+
+  after :all do
+    Process.kill(:INT, Process.pid)
+    @server_thread.join
   end
 
   subject { @yumrepo }
@@ -21,17 +41,15 @@ rpm "basesystem"
 
   describe "yumfile" do
     subject { @yumrepo.yumfile }
-    its(:repos) { should have_key "base" }
-    its(:rpms)  { should have_key "basesystem" }
+    its(:repos) { should have_key "test" }
+    its(:rpms)  { should have_key "pandler-test" }
   end
 
   describe "createrepo" do
     before :all do
       @yumrepo.createrepo
     end
-#    it("should have repodata") { Pathname("#{@yumrepo.repo_dir}/repodata").should exist }
-    it do
-      system "tree repo"
-    end
+    it("should have repodata") { Pathname("#{@yumrepo.repo_dir}/repodata").should exist }
+    it("should have pandle-test") { Pathname("#{@yumrepo.repo_dir}/pandler-test-0.0.1-1.x86_64.rpm").should exist }
   end
 end
